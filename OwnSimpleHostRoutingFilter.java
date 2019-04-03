c969b827b54049dc076f1499a40dcf4d9fc5577f
miracle6037 <603763193@qq.com>
2019-04-03 10:14:56 +0800

    Create OwnSimpleHostRoutingFilter.java

---
+++ OwnSimpleHostRoutingFilter.java
@@ -0,0 +1,162 @@
+@Slf4j
+@Component
+public class OwnSimpleHostRoutingFilter extends SimpleHostRoutingFilter {
+
+    private ProxyRequestHelper  helper;
+
+    @Autowired
+    @Qualifier("ownHttpClient")
+    private CloseableHttpClient httpClient;
+
+    public OwnSimpleHostRoutingFilter(ProxyRequestHelper helper, ZuulProperties properties,
+                                      ApacheHttpClientConnectionManagerFactory connectionManagerFactory,
+                                      ApacheHttpClientFactory httpClientFactory) {
+        super(helper, properties, connectionManagerFactory, httpClientFactory);
+        this.helper = helper;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        MultiValueMap<String, String> headers = this.helper.buildZuulRequestHeaders(request);
+        MultiValueMap<String, String> params = this.helper.buildZuulRequestQueryParams(request);
+        String verb = getVerb(request);
+        InputStream requestEntity = getRequestBody(request);
+        if (getContentLength(request) < 0) {
+            context.setChunkedRequestBody();
+        }
+
+        String uri = this.helper.buildZuulRequestURI(request);
+        this.helper.addIgnoredHeaders();
+
+        try {
+            CloseableHttpResponse response = forward(this.httpClient, verb, uri, request, headers, params,
+                    requestEntity);
+            setResponse(response);
+        } catch (Exception ex) {
+            throw new ZuulRuntimeException(ex);
+        }
+        return null;
+    }
+
+    private CloseableHttpResponse forward(CloseableHttpClient httpclient, String verb, String uri,
+                                          HttpServletRequest request, MultiValueMap<String, String> headers,
+                                          MultiValueMap<String, String> params, InputStream requestEntity)
+            throws Exception {
+        Map<String, Object> info = this.helper.debug(verb, uri, headers, params, requestEntity);
+        URL host = RequestContext.getCurrentContext().getRouteHost();
+        HttpHost httpHost = getHttpHost(host);
+        uri = StringUtils.cleanPath((host.getPath() + uri).replaceAll("/{2,}", "/"));
+        long contentLength = getContentLength(request);
+
+        ContentType contentType = null;
+
+        if (request.getContentType() != null) {
+            contentType = ContentType.parse(request.getContentType());
+        }
+
+        InputStreamEntity entity = new InputStreamEntity(requestEntity, contentLength, contentType);
+
+        HttpRequest httpRequest = buildHttpRequest(verb, uri, entity, headers, params, request);
+        try {
+            log.debug(httpHost.getHostName() + " " + httpHost.getPort() + " " + httpHost.getSchemeName());
+            CloseableHttpResponse zuulResponse = forwardRequest(httpclient, httpHost, httpRequest);
+            this.helper.appendDebug(info, zuulResponse.getStatusLine().getStatusCode(),
+                    revertHeaders(zuulResponse.getAllHeaders()));
+            return zuulResponse;
+        } finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            // httpclient.getConnectionManager().shutdown();
+        }
+    }
+
+    @Override
+    protected HttpRequest buildHttpRequest(String verb, String uri, InputStreamEntity entity,
+                                           MultiValueMap<String, String> headers, MultiValueMap<String, String> params,
+                                           HttpServletRequest request) {
+        HttpRequest httpRequest;
+        String uriWithQueryString = uri + (true ? getEncodedQueryString(request) : this.helper.getQueryString(params));
+
+        switch (verb.toUpperCase()) {
+            case "POST":
+                HttpPost httpPost = new HttpPost(uriWithQueryString);
+                httpRequest = httpPost;
+                httpPost.setEntity(entity);
+                break;
+            case "PUT":
+                HttpPut httpPut = new HttpPut(uriWithQueryString);
+                httpRequest = httpPut;
+                httpPut.setEntity(entity);
+                break;
+            case "PATCH":
+                HttpPatch httpPatch = new HttpPatch(uriWithQueryString);
+                httpRequest = httpPatch;
+                httpPatch.setEntity(entity);
+                break;
+            case "DELETE":
+                BasicHttpEntityEnclosingRequest entityRequest = new BasicHttpEntityEnclosingRequest(verb,
+                        uriWithQueryString);
+                httpRequest = entityRequest;
+                entityRequest.setEntity(entity);
+                break;
+            default:
+                httpRequest = new BasicHttpRequest(verb, uriWithQueryString);
+                log.debug(uriWithQueryString);
+        }
+
+        httpRequest.setHeaders(convertHeaders(headers));
+        return httpRequest;
+    }
+
+    private String getEncodedQueryString(HttpServletRequest request) {
+        String query = request.getQueryString();
+        return (query != null) ? "?" + query : "";
+    }
+
+    private MultiValueMap<String, String> revertHeaders(Header[] headers) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        for (Header header : headers) {
+            String name = header.getName();
+            if (!map.containsKey(name)) {
+                map.put(name, new ArrayList<String>());
+            }
+            map.get(name).add(header.getValue());
+        }
+        return map;
+    }
+
+    private Header[] convertHeaders(MultiValueMap<String, String> headers) {
+        List<Header> list = new ArrayList<>();
+        for (String name : headers.keySet()) {
+            for (String value : headers.get(name)) {
+                list.add(new BasicHeader(name, value));
+            }
+        }
+        return list.toArray(new BasicHeader[0]);
+    }
+
+    private CloseableHttpResponse forwardRequest(CloseableHttpClient httpclient, HttpHost httpHost,
+                                                 HttpRequest httpRequest)
+            throws IOException {
+        return httpclient.execute(httpHost, httpRequest);
+    }
+
+    private HttpHost getHttpHost(URL host) {
+        HttpHost httpHost = new HttpHost(host.getHost(), host.getPort(), host.getProtocol());
+        return httpHost;
+    }
+
+    private String getVerb(HttpServletRequest request) {
+        String sMethod = request.getMethod();
+        return sMethod.toUpperCase();
+    }
+
+    private void setResponse(HttpResponse response) throws IOException {
+        RequestContext.getCurrentContext().set("zuulResponse", response);
+        this.helper.setResponse(response.getStatusLine().getStatusCode(),
+                response.getEntity() == null ? null : response.getEntity().getContent(),
+                revertHeaders(response.getAllHeaders()));
+    }
